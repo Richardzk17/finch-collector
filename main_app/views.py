8a25060809ad810395d492f846353d661954016f
richardzk17 <richardhernandezk17@gmail.com>
2024-02-27 22:17:10 -0500

    lab part 2

--- main_app/views.py
+++ main_app/views.py
@@ -1,26 +1,18 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Finch
 
-class Finch:
-  def __init__(self, name, species, description, age):
-    self.name = name
-    self.species = species
-    self.description = description
-    self.age = age
-
-finches = [
-  Finch('Tweetie', 'zebra finch', 'Chirps melodiously.', 2),
-  Finch('Pico', 'java sparrow', 'Has beautiful plumage.', 1),
-  Finch('Chirpy', 'canary', 'Loves to sing.', 3),
-  Finch('Pepper', 'society finch', 'Very sociable.', 1)
-]
 
 
 def home(request):
-  return HttpResponse('<h1>Hello ᓚᘏᗢ</h1>')
+  return render(request, 'home.html')
 
 def about(request):
   return render(request, 'about.html')
 
 def finch_index(request):
+  finches = Finch.objects.all()
   return render(request, 'finches/index.html', { 'finches': finches })
+
+def finch_detail(request, finch_id):
+  finch = Finch.objects.get(id=finch_id)
+  return render(request, 'finches/detail.html', { 'finch': finch })
