9abbbd00a2a02736ef5355e30c45682912cf9d75
richardzk17 <richardhernandezk17@gmail.com>
2024-02-29 09:00:23 -0500

    part 5 done

--- main_app/models.py
+++ main_app/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.urls import reverse
+from datetime import date
 
 
 MEALS = (
@@ -29,6 +30,8 @@ class Feeding(models.Model):
 		choices=MEALS,
 		default=MEALS[0][0]
   )
+  def fed_for_today(self):
+    return self.feeding_set.filter(date=date.today()).count() >= len(MEALS)
 
   Finch = models.ForeignKey(Finch, on_delete=models.CASCADE)
 
@@ -37,3 +40,15 @@ class Feeding(models.Model):
   
 class Meta:
   ordering = ['-date']
+
+
+# Add the Toy model
+class Toy(models.Model):
+  name = models.CharField(max_length=50)
+  color = models.CharField(max_length=20)
+
+  def __str__(self):
+    return self.name
+
+  def get_absolute_url(self):
+    return reverse('toy-detail', kwargs={'pk': self.id})
